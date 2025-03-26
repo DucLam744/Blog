2aaafd2a2517c772184d82375ecb927d58556c71
Nguyễn Đức Mạnh <dmanhpt@gmail.com>
2025-03-26 11:02:55 +0700

    res

--- src/components/editor/Editor.js
+++ src/components/editor/Editor.js
@@ -1,27 +1,27 @@
-import React, { useState } from "react"
-import { Editor } from "@tinymce/tinymce-react"
+// import React, { useState } from "react"
+// // import { Editor } from "@tinymce/tinymce-react";
 
-const BlogEditor = ({ value, onChange }) => {
-  const handleEditorChange = (content) => {
-    onChange(content)
-  }
+// const BlogEditor = ({ value, onChange }) => {
+//   const handleEditorChange = (content) => {
+//     onChange(content)
+//   }
 
-  return (
-    <div>
-      <h2>Write your blog post</h2>
-      <Editor
-        value={value}
-        onEditorChange={handleEditorChange}
-        init={{
-          height: 500,
-          menubar: false,
-          plugins: ["link", "lists", "image"],
-          toolbar:
-            "undo redo | bold italic | alignleft aligncenter alignright | bullist numlist outdent indent | link image",
-        }}
-      />
-    </div>
-  )
-}
+//   return (
+//     <div>
+//       <h2>Write your blog post</h2>
+//       <Editor
+//         value={value}
+//         onEditorChange={handleEditorChange}
+//         init={{
+//           height: 500,
+//           menubar: false,
+//           plugins: ["link", "lists", "image"],
+//           toolbar:
+//             "undo redo | bold italic | alignleft aligncenter alignright | bullist numlist outdent indent | link image",
+//         }}
+//       />
+//     </div>
+//   )
+// }
 
-export default BlogEditor
+// export default BlogEditor
